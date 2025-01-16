3862522838465bbe22e8d526cad1fa16098d103c
Mohamad Meksasi <45299765+mohamadmek@users.noreply.github.com>
2025-01-16 19:56:10 +0100

    .

--- workmanager-blur/app/build.gradle.kts
+++ workmanager-blur/app/build.gradle.kts
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -20,11 +21,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.dessertrelease"
+    namespace = "com.example.bluromatic"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.dessertrelease"
+        applicationId = "com.example.bluromatic"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
@@ -66,16 +67,20 @@ dependencies {
 
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
     implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.activity:activity-ktx:1.9.3")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.runtime:runtime")
+    implementation("androidx.compose.runtime:runtime-livedata")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:${rootProject.extra["lifecycle_version"]}")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:${rootProject.extra["lifecycle_version"]}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
-
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-
+    implementation("androidx.work:work-runtime-ktx:2.10.0")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
