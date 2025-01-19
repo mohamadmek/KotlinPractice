e06df235efdf0778bcf6cc6c8026de0d2f4abfa5
Mohamad Meksasi <45299765+mohamadmek@users.noreply.github.com>
2025-01-19 21:02:47 +0100

    .

--- BankingUi/app/build.gradle.kts
+++ BankingUi/app/build.gradle.kts
@@ -56,4 +56,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.27.0")
+
+    // Extended Icons
+    implementation("androidx.compose.material:material-icons-extended:1.5.4")
 }
